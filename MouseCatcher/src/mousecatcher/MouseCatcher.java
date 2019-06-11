2c2eaf883acc1da97e452947686de159cc18b3b7
Ben <benf89@gmail.com>
2019-06-11 14:16:22 -0400

    Add files via upload

---
+++ MouseCatcher/src/mousecatcher/MouseCatcher.java
@@ -0,0 +1,15 @@
+/**
+ * @author Ben Forgy at touro
+ * 11/13/13
+ */
+package mousecatcher;
+
+public class MouseCatcher {
+
+    public static void main(String[] args) {
+       
+        MouseCatcherGame mouseCatcherGame = new MouseCatcherGame(); 
+        mouseCatcherGame.start();
+        System.exit(0);
+    }
+}
---
+++ MouseCatcher/test/mousecatcher/MouseCatcherTest.java
@@ -0,0 +1,22 @@
+package mousecatcher;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ben Forgy at touro
+ */
+public class MouseCatcherTest {
+
+    @Test
+    public void pointList()
+    {
+        PointList list = new PointList(8);
+    }
+}
